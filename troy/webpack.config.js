d8700bccecb1a91fc26f7560fcb91cd8997f0ca7
Lui <ruitang307@gmail.com>
2016-03-06 21:13:11 +0800

    分离css

--- troy/webpack.config.js
+++ troy/webpack.config.js
@@ -1,12 +1,15 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
-	entry: './app.js',
-	output: {
-		path: './dist',
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{test: /\.scss$/, loader: 'style!css!sass'}
-		]
-	}
+  entry: './app.js',
+  output: {
+    path: './dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{ test: /\.scss$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader", "sass-loader") }]
+  },
+    plugins: [
+        new ExtractTextPlugin("styles.css")
+    ]
 }
